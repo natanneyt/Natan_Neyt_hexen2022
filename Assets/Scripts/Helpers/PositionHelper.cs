50b6b1d0d37425c6ca58c564ee9d91b17f638bdb
natanneyt <neytnatan@gmail.com>
2023-01-16 08:35:02 +0100

    Finished version before exam.

--- Assets/Scripts/Helpers/PositionHelper.cs
+++ Assets/Scripts/Helpers/PositionHelper.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public static class PositionHelper
 {
--- Assets/Scripts/RowAttackCard.cs
+++ Assets/Scripts/RowAttackCard.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEngine;
 
 public class RowAttackCard : MoveSet
 {
